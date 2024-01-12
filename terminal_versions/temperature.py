3cedba13db52bf8d596095ff064a0664ded47de9
Felix Jobi <107713717+An-AvG-Geek@users.noreply.github.com>
2024-01-12 14:35:27 +0530

    Update temperature.py

--- terminal_versions/temperature.py
+++ terminal_versions/temperature.py
@@ -1,10 +1,12 @@
-# menu driven python program for converting different temperatures.
+# menu driven python program for converting different temperatures
 
 
 def main():
     while True:
         print()
-        print("***************************************************************************")
+        print(
+            "***************************************************************************"
+        )
         print(
             """
             \t\tmenu
@@ -16,42 +18,72 @@ def main():
               6.kelvin to farenheit
               7.exit from program\n"""
         )
-        print("***************************************************************************")
+        print(
+            "***************************************************************************"
+        )
 
-        choice = int(input("Enter the choice "))
+        try:
+            choice = int(input("Enter the choice "))
+        except ValueError:
+            print("Enter a valid choice....\n")
+            continue
 
         if choice == 1:
-            celcius = float(input("Enter the temperature in celsius "))
+            try:
+                celcius = float(input("Enter the temperature in celsius "))
+            except ValueError:
+                print("Enter a valid temperature....\n")
+                continue
 
             farenheit = (celcius * 9 / 5) + 32
             print(f"the temperature in farenheit is {farenheit}")
 
         elif choice == 2:
-            farenheit = float(input("Enter the temperature in farenheit "))
+            try:
+                farenheit = float(input("Enter the temperature in farenheit "))
+            except ValueError:
+                print("Enter a valid temperature ....\n")
+                continue
 
             celsius = (farenheit - 32) * 5 / 9
             print(f"the temperature in celsius is {celsius}")
 
         elif choice == 3:
-            celsius = float(input("Enter the temperature in celsius "))
+            try:
+                celsius = float(input("Enter the temperature in celsius "))
+            except ValueError:
+                print("Enter a valid temperature ....\n")
+                continue
 
             kelvin = celsius + 273.15
             print(f"the temperature in kelvin is {kelvin}")
 
         elif choice == 4:
-            kelvin = float(input("Enter the temperature in kelvin "))
+            try:
+                kelvin = float(input("Enter the temperature in kelvin "))
+            except ValueError:
+                print("Enter a valid temperature ....\n")
+                continue
 
             celsius = kelvin - 273.15
             print(f"the temperature in celsius is {celsius}")
 
         elif choice == 5:
-            farenheit = float(input("Enter the temperature in farenheit "))
+            try:
+                farenheit = float(input("Enter the temperature in farenheit "))
+            except ValueError:
+                print("Enter a valid temperature ....\n")
+                continue
 
             kelvin = ((farenheit - 32) * (5 / 9)) + 273.15
             print(f"the temperature in kelvin is {kelvin}")
 
         elif choice == 6:
-            kelvin = float(input("Enter the temperature in kelvin "))
+            try:
+                kelvin = float(input("Enter the temperature in kelvin "))
+            except ValueError:
+                print("Enter a valid temperature ....\n")
+                continue
 
             farenheit = ((kelvin - 273.15) * (9 / 5)) + 32
             print(f"the temperature in farenheit is {farenheit}")
